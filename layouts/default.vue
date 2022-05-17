4c4199b2bf126ce2e0fbd986a7c09ffacd3554e9
Leonid Rezvitsky <rezvitsky@swert.ru>
2022-05-17 22:40:52 +0700

    upload

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nav class="flex align-center gap-4 p-4">
+      <NuxtLink to="/">
+        Index
+      </NuxtLink>
+      <NuxtLink to="/welcome">
+        Welcome
+      </NuxtLink>
+    </nav>
+
+    <slot />
+    
+  </div>
+</template>
